c0bbba302e9ea7849b51bef017ecfe280236c4bf
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-08-16 04:16:35 +0200

    Create profil_recruteur.php

---
+++ profil_recruteur.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TRT Conseil - Profil Recruteur</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Profil Recruteur</h1>
+        <form action="completer_profil.php" method="post">
+            <input type="text" name="nom_entreprise" placeholder="Nom de l'entreprise" required>
+            <input type="text" name="adresse_entreprise" placeholder="Adresse de l'entreprise" required>
+            <button type="submit">Compléter le profil</button>
+        </form>
+    </div>
+</body>
+</html>
