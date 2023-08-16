714d3a26e4ed51ffdbe12818a32b62a5a2cc4813
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-08-17 01:11:54 +0200

    Create confirmation.php

---
+++ confirmation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRT Conseil - Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>TRT Conseil - Plateforme de Recrutement</h1>
+        <p>La solution pour trouver le bon emploi dans l'hôtellerie et la restauration.</p>
+    </header>
+<main>
+        <section class="confirmation">
+            <h2>Inscription réussie</h2>
+            <p>Merci pour votre inscription. Votre demande est en cours de traitement.</p>
+            <p>Un consultant examinera votre demande et vous recevrez un e-mail dès que votre compte sera activé.</p>
+            <p>Vous pouvez vous <a href="connexion.php">connecter ici</a> dès que votre compte est actif.</p>
+        </section>
+    </main>
+<footer>
+        <p>&copy; 2023 TRT Conseil. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
