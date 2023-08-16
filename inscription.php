1cb6bbaf825c94614628fccdd431fc48426c7ae5
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-08-17 01:03:56 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,70 +1,41 @@
-<?php
-// Chemin vers votre base de données SQLite sur Repl.it
-$db_path = $_SERVER['DOCUMENT_ROOT'] . "/chemin/vers/votre/base_de_donnees.sqlite";
-
-// Connexion à la base de données
-$conn = new SQLite3($db_path);
-
-// Code d'inscription
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $mot_de_passe = $_POST["mot_de_passe"];
-    $confirmation_mot_de_passe = $_POST["confirmation_mot_de_passe"];
-    $type_utilisateur = $_POST["type_utilisateur"];
-
-    // Vérifier si le mot de passe correspond à la confirmation
-    if ($mot_de_passe != $confirmation_mot_de_passe) {
-        $error_message = "Les mots de passe ne correspondent pas.";
-    } else {
-        // Hasher le mot de passe
-        $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
-
-        // Insérer l'utilisateur dans la base de données
-        $stmt = $conn->prepare("INSERT INTO Utilisateurs (email, mot_de_passe, type_utilisateur, statut) VALUES (?, ?, ?, ?)");
-        $statut = "en attente d'approbation"; // Statut par défaut pour l'approbation par un consultant
-        $stmt->bind_param("ssss", $email, $mot_de_passe_hache, $type_utilisateur, $statut);
-
-        if ($stmt->execute()) {
-            $success_message = "Inscription réussie. En attente d'approbation.";
-        } else {
-            $error_message = "Erreur lors de l'inscription.";
-        }
-    }
-}
-
-// Fermeture de la connexion à la base de données
-$conn->close();
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-    <title>Inscription - TRT Conseil</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRT Conseil - Accueil</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Inscription</h1>
-        <?php
-        if (isset($error_message)) {
-            echo "<p class='error'>$error_message</p>";
-        }
-        if (isset($success_message)) {
-            echo "<p class='success'>$success_message</p>";
-        }
-        ?>
-        <form action="inscription.php" method="post">
-            <input type="email" name="email" placeholder="Adresse e-mail" required>
-            <input type="password" name="mot_de_passe" placeholder="Mot de passe" required>
-            <input type="password" name="confirmation_mot_de_passe" placeholder="Confirmez le mot de passe" required>
-            <select name="type_utilisateur">
-                <option value="candidat">Candidat</option>
-                <option value="recruteur">Recruteur</option>
-                <option value="consultant">Consultant</option>
-            </select>
-            <button type="submit">S'inscrire</button>
-        </form>
-        <p>Vous avez déjà un compte ? <a href="index.php">Connectez-vous</a></p>
-    </div>
+    <header>
+ <h1>TRT Conseil - Plateforme de Recrutement</h1>
+        <p>La solution pour trouver le bon emploi dans l'hôtellerie et la restauration.</p>
+    </header>
+    <main>
+        <section class="auth-form">
+            <h2>Inscription</h2>
+            <form action="traitement_inscription.php" method="POST">
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" required>
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required>
+                <label for="userType">Type d'utilisateur :</label>
+                <select id="userType" name="userType" required>
+                    <option value="recruteur">Recruteur</option>
+                    <option value="candidat">Candidat</option>
+                </select>
+                <label for="firstName">Prénom :</label>
+                <input type="text" id="firstName" name="firstName" required>
+                <label for="lastName">Nom :</label>
+                <input type="text" id="lastName" name="lastName" required>
+                <label for="birthdate">Date de naissance :</label>
+                <input type="date" id="birthdate" name="birthdate" required>
+                <button type="submit">S'inscrire</button>
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 TRT Conseil. Tous droits réservés.</p>
+    </footer>
 </body>
 </html>
